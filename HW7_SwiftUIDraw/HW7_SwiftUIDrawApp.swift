12cb5d527c7240f1ed2a62868a5db27ffd6384ad
tommy <tommy5566123@gmail.com>
2021-11-18 14:33:10 +0800

    Initial Commit

---
+++ HW7_SwiftUIDraw/HW7_SwiftUIDrawApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW7_SwiftUIDrawApp.swift
+//  HW7_SwiftUIDraw
+//
+//  Created by 蔡尚諺 on 2021/11/18.
+//
+
+import SwiftUI
+
+@main
+struct HW7_SwiftUIDrawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
